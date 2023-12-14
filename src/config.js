f01c538f08b93e9ac6a4ed339409a738bc3b3491
Sylvester Keil <sylvester@keil.or.at>
2023-12-14 15:57:01 +0100

    Load package.json file without input attributes

--- src/config.js
+++ src/config.js
@@ -1,21 +1,29 @@
-import fs from 'node:fs'
+import { readFile } from 'node:fs/promises'
 import { join } from 'node:path'
 import { cwd, env } from 'node:process'
-import pkg from '../package.json' with { type: 'json' }
+
+async function readJSON(file) {
+  try {
+    return JSON.parse(
+      await readFile(file, { encoding: 'utf-8' })
+    )
+
+  } catch (e) {
+    if (e.code !== 'ENOENT') {
+      console.warn(`failed opening ${file}: ${e.message}`)
+    }
+  }
+}
+
+const pkg = await readJSON(new URL('../package.json', import.meta.url))
 
 export const config = {
   api: 'https://transkribus.eu/TrpServer/rest',
   userAgent: `${pkg.productName || pkg.name}/${pkg.version}`
 }
 
-try {
-  Object.assign(config, JSON.parse(
-    fs.readFileSync(join(cwd(), '.artimirc.json'), {
-      encoding: 'utf-8'
-    })))
-} catch {
-  // ignore
-}
+const rc = await readJSON(join(cwd(), '.artimirc.json'))
+if (rc) Object.assign(config, rc)
 
 if (env.TRANSKRIBUS_USER)
   config.user = env.TRANSKRIBUS_USER
