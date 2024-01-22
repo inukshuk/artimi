91b7a2b6f2e180ee849a190d9de0ef8e1b3f0a91
Sylvester Keil <sylvester@keil.or.at>
2024-01-22 15:56:41 +0100

    Support using buffer directly

--- src/image.js
+++ src/image.js
@@ -5,6 +5,7 @@ import { URL, pathToFileURL } from 'node:url'
 const PROTO = /^[a-z]+:\/\//i
 
 export class Image {
+  #buffer
   #url
 
   static async open(input) {
@@ -12,10 +13,20 @@ export class Image {
   }
 
   constructor(input) {
+    if (input instanceof Buffer)
+      this.buffer = input
     if (input instanceof URL || PROTO.test(input))
       this.url = input
     else
       this.path = input
+  }
+
+  set buffer(buffer) {
+    this.#buffer = Buffer.from(buffer)
+  }
+
+  get buffer() {
+    return this.#buffer
   }
 
   set path(path) {
@@ -41,7 +52,7 @@ export class Image {
         if (!res.ok) {
           throw new Error(`cannot open ${this.url}: ${res.status}`)
         }
-        this.buffer = Buffer.from(await res.arrayBuffer())
+        this.buffer = await res.arrayBuffer()
         break
       }
       default:
@@ -52,7 +63,7 @@ export class Image {
   }
 
   toJSON() {
-    if (this.buffer) {
+    if (this.buffer != null) {
       return {
         base64: this.buffer.toString('base64')
       }
