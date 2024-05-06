cfbb496a86d98eb837dd4b76466e99221e9d7cec
Sylvester Keil <sylvester@keil.or.at>
2024-05-06 19:58:15 +0200

    Fix status enums

--- src/process.js
+++ src/process.js
@@ -1,18 +1,16 @@
-import assert from 'node:assert/strict'
 import { EventEmitter } from 'node:events'
 
-export const Status = Object.freeze(Object.create({}, {
-  CREATED: 'CREATED',
-  FAILED: 'FAILED',
-  FINISHED: 'FINISHED',
-  RUNNING: 'RUNNING',
-  WAITING: 'WAITING'
-}))
+
+const CREATED = 'CREATED'
+const FAILED = 'FAILED'
+const FINISHED ='FINISHED'
+// const RUNNING = 'RUNNING'
+// const WAITING = 'WAITING'
 
 export class Process extends EventEmitter {
   #status
 
-  constructor(id, status = Status.CREATED) {
+  constructor(id, status = CREATED) {
     super({ captureRejections: true })
 
     this.id = id
@@ -24,13 +22,11 @@ export class Process extends EventEmitter {
   }
 
   set status(status) {
-    assert(Status.keys().includes(status), `unknown status: ${status}`)
     this.#status = status
   }
 
   get done() {
-    return this.#status === Status.FINISHED ||
-      this.#status === Status.FAILED
+    return this.#status === FINISHED || this.#status === FAILED
   }
 
   update({ status, content }) {
@@ -44,10 +40,10 @@ export class Process extends EventEmitter {
     this.emit('change', status, prev, this)
 
     switch (status) {
-      case Status.FINISHED:
+      case FINISHED:
         this.emit('done', this)
         break
-      case Status.FAILED:
+      case FAILED:
         this.emit('error', new Error(`process#${this.id} failed`))
         break
     }
