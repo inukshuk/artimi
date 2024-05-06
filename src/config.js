9832f73490a1620d65f45843ffd518cebdd62bc8
Sylvester Keil <sylvester@keil.or.at>
2024-05-06 16:07:16 +0200

    Bring back import assertion for loading package.json
    Ignore the file in eslint config for now

--- src/config.js
+++ src/config.js
@@ -1,30 +1,15 @@
 import { readFile } from 'node:fs/promises'
 import { join } from 'node:path'
 import { cwd, env } from 'node:process'
-
-async function readJSON(file) {
-  try {
-    return JSON.parse(
-      await readFile(file, { encoding: 'utf-8' })
-    )
-  } catch (e) {
-    if (e.code !== 'ENOENT') {
-      console.warn(`failed opening ${file}: ${e.message}`)
-    }
-  }
-}
-
-const pkg = await readJSON(new URL('../package.json', import.meta.url))
+import pkg from '../package.json' with { type: 'json' }
 
 export const config = {
   auth: 'https://account.readcoop.eu/auth/realms/readcoop/protocol/openid-connect',
+  delay: 10000,
   metagrapho: 'https://transkribus.eu/processing/v1',
   trp: 'https://transkribus.eu/TrpServer/rest',
   userAgent: `${pkg.productName || pkg.name}/${pkg.version}`
 }
-
-const rc = await readJSON(join(cwd(), '.artimirc.json'))
-if (rc) Object.assign(config, rc)
 
 if (env.TRANSKRIBUS_USER)
   config.user = env.TRANSKRIBUS_USER
