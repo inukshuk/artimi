35eb08ee9a91457c16eff4dc8e528d4b288ed40c
Sylvester Keil <sylvester@keil.or.at>
2024-05-06 19:58:39 +0200

    Just test endpoint presence

--- test/config_test.js
+++ test/config_test.js
@@ -2,6 +2,8 @@ import assert from 'node:assert'
 import test from 'node:test'
 import defaults from '../src/config.js'
 
-test('defines an api endpoint', () => {
-  assert.ok(defaults.api)
+test('defines api and auth endpoints', () => {
+  assert.ok(defaults.auth)
+  assert.ok(defaults.metagrapho)
+  assert.ok(defaults.trp)
 })
