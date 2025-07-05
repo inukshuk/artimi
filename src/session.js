5e1f8f52b01f131f85c806dee28a5d47f617532a
Sylvester Keil <sylvester@keil.or.at>
2025-07-05 18:08:11 +0200

    Use console in verbose mode

--- src/session.js
+++ src/session.js
@@ -11,8 +11,8 @@ function urlSearchParams (obj) {
 }
 
 export class Session {
-  constructor ({ logger, ...options }) {
-    this.logger = logger
+  constructor ({ logger, verbose, ...options }) {
+    this.logger = logger || verbose ? console : undefined
     this.config = {
       ...defaults,
       ...options
